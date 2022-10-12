d397c6c9ef7ed69c6b4bb28d2279d25bf20019fd
Marc <37132937+ctrl-f-marc@users.noreply.github.com>
2022-10-12 10:35:24 -0700

    Add files via upload

---
+++ MyLifeGoals.java
@@ -0,0 +1,11 @@
+
+public class MyLifeGoals {
+    public static void main(String[] args) {
+        System.out.println("My name is Marc");
+        System.out.println();
+        System.out.println("My short-term goal is to pass this class with a good grade.");
+        System.out.println("My medium-term goal is to successfully graduate from this program with a " +
+                "4.0 gpa.");
+        System.out.println("My long-term goal is to make enough money to own some piece of property.");
+    }
+}
